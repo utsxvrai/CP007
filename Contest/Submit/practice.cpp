11bdf86a8fba63edbbf21239cdf254a62b2fc979
utsxvrai <utsavrai2115@gmail.com>
2023-08-31 23:58:10 +0530

    31Aug

--- Contest/Submit/practice.cpp
+++ Contest/Submit/practice.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
-#include <cmath>
-using namespace std;
+#include <map>
 
 int main() {
-    int t;
-    cin >> t;
-
-    while (t--) {
-        long long d;
-        cin >> d;
-        
-
-        
+    int n;
+    std::cin >> n;
+    
+    std::map<int, int> home_colors;
+    std::map<int, int> guest_colors;
+    
+    for (int i = 0; i < n; ++i) {
+        int h, a;
+        std::cin >> h >> a;
+        home_colors[h]++;
+        guest_colors[a]++;
     }
-
+    
+    int total_guest_uniforms = 0;
+    
+    for (const auto& entry : home_colors) {
+        int h = entry.first;
+        int a_count = entry.second;
+        total_guest_uniforms += guest_colors[h] * a_count;
+    }
+    
+    std::cout << total_guest_uniforms << std::endl;
+    
     return 0;
 }
